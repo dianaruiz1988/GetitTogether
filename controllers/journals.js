531d9462caa91414b4876bd5e9f5ac48f6d49579
dianaruiz1988 <druiz2627@gmail.com>
2022-03-11 13:09:47 -0600

    added seed route for testing

--- controllers/journals.js
+++ controllers/journals.js
@@ -8,6 +8,45 @@ const router = express.Router();
 
 //ROUTES:===========================================================================
 
+//SEED++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+router.get('/seed', (req, res) => {
+    const startJournals = [
+        {
+            date: "3/11/22",
+            grateful: "grateful for family",
+            affirmations: " I will do good on this project",
+            today: "today I am grateful for opportunies heading my way",
+            tomorrow: "tomorrow I will be grateful to have time to work on project",
+            brushTeeth: true,
+            washFace: true,
+            eatMeal: true,
+            getFreshAir: false,
+            drinkWater: true,
+        },
+        {
+            date: "3/12/22",
+            grateful: "grateful for health",
+            affirmations: " I will believe in myself",
+            today: "today I am grateful for being able to work on the project",
+            tomorrow: "tomorrow I will be grateful to have even more time",
+            brushTeeth: true,
+            washFace: true,
+            eatMeal: true,
+            getFreshAir: false,
+            drinkWater: true,
+        },
+    ];
+    Journal.deleteMany({}).then((data) => {
+        Journal.create(startJournals).then((data) => {
+            res.json(data);
+        })
+    }).catch((err) => {
+        res.status(400).send(err)
+    })
+})
+
+
+
 //INDUCES===========================================================================
 
 //INDEX+++++++++++++++++++++++
