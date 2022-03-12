bb1614fcbeccd7ef8892bce32319136aef9fcff3
dianaruiz1988 <druiz2627@gmail.com>
2022-03-12 13:52:50 -0600

    did taking off script do anything

--- views/Default.jsx
+++ views/Default.jsx
@@ -11,14 +11,16 @@ class DefaultLayout extends React.Component {
                     integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"></link>
                     <meta httpEqiv="X-UA-Compatbile" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Get it Together Website</title>
-                    <script
+                    <title>Get it Together</title>
+                    {/* <script
                         src="https://code.jquery.com/jquery-3.6.0.min.js"
                         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-                        crossOrigin="anonymous"
-                    ></script>
+                        crossOrigin="anonymous">
+                    </script> */}
                 </head>
-
+                <body>
+                    <div id="main"></div>
+                </body>
             </html>
         )
     }
