73155dde27e5a0fa6279eff059f3296a7aa0c837
dianaruiz1988 <druiz2627@gmail.com>
2022-03-11 19:19:55 -0600

    playing with style

--- views/Default.jsx
+++ views/Default.jsx
@@ -14,14 +14,8 @@ class DefaultLayout extends React.Component {
                         rel='stylesheet'
                         href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
                     />
-                    <link
-                        rel="stylesheet"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
-                    />
-                    <link
-                        rel="stylesheet"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"
-                    />
+
+
                     <script
                         src="https://code.jquery.com/jquery-3.6.0.min.js"
                         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
