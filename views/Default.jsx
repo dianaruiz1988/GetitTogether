881db995b0a945266bba77ef72807f7cc462eb41
dianaruiz1988 <druiz2627@gmail.com>
2022-03-11 20:57:43 -0600

    need to expedite, behind

--- views/Default.jsx
+++ views/Default.jsx
@@ -7,15 +7,10 @@ class DefaultLayout extends React.Component {
             <html lang="en">
                 <head>
                     <meta charSet='UTF-8' />
+                    <link rel="stylesheet" href="/css/style.css" charset="utf-8"></link>
                     <meta httpEqiv="X-UA-Compatbile" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <title>Get it Together Website</title>
-                    <link
-                        rel='stylesheet'
-                        href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
-                    />
-
-
                     <script
                         src="https://code.jquery.com/jquery-3.6.0.min.js"
                         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
