c403262e8519473a3fe6ef75e766476bc10cb5bc
dianaruiz1988 <druiz2627@gmail.com>
2022-03-12 13:25:19 -0600

    changing database to see if it will seed correctly

--- views/journals/New.jsx
+++ views/journals/New.jsx
@@ -8,7 +8,7 @@ class New extends React.Component {
                         <legend>Create a New Journal Entry</legend>
                         
                         DATE: <input name="date"  type="text"  placeholder="enter date like this month/day/year" /><br />
-                        TITLE: <input name="title"  type="text"  placeholder="enter date like this month/day/year" /><br />
+                        TITLE: <input name="title"  type="text"  placeholder="enter a title for your journal entry" /><br />
                         I AM GRATEFUL FOR: <br /><textarea name="grateful" type="textarea" /><br />
                         DAILY AFFIRMATION: <br /><textarea name="affirmations" type="textarea" /><br />
                         TODAY: <br /><textarea name="brushTeeth" type="textarea" /><br />
@@ -17,16 +17,6 @@ class New extends React.Component {
                         BRUSH TEETH:<input name="shipIsBroken" type="checkbox" /><br />
                     
                       
-
-
-
-
-
-
-
-
-
-
 
                     
                     </fieldset>
