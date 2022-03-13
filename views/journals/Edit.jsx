af98bf1a4aff623ab88aa5cf9a82a90526b35e35
dianaruiz1988 <druiz2627@gmail.com>
2022-03-13 18:18:09 -0500

    edit form revision

--- views/journals/Edit.jsx
+++ views/journals/Edit.jsx
@@ -6,21 +6,20 @@ class Edit extends React.Component {
         return (
                   <form action={`/journals/${journal._id}?_method=PUT`} method="post">
                     <fieldset>
-                        <legend>Create a New Journal Entry</legend>
+                        <legend>Edit this Journal Entry</legend>
                         
-                        DATE: <input name="date"  type="text"  placeholder="enter date like this month/day/year" /><br />
-                        TITLE: <input name="title"  type="text"  placeholder="enter a title for your journal entry" /><br />
-                        I AM GRATEFUL FOR: <br /><textarea name="grateful" type="textarea" /><br />
-                        AFFIRMATION: <br /><textarea name="affirmations" type="textarea" /><br />
-                        TODAY: <br /><textarea name="today" type="textarea" /><br />
-                        TOMORROW: <br /><textarea name="tomorrow" type="textarea" /><br />
+                        DATE: <input name="date"  type="text"  placeholder="enter date like this month/day/year" defaultValue={journal.date} /><br />
+                        TITLE: <input name="title"  type="text"  placeholder="enter a title for your journal entry" defaultValue={journal.title} /><br />
+                        I AM GRATEFUL FOR: <br /><textarea name="grateful" type="textarea" defaultValue={journal.grateful} /><br />
+                        AFFIRMATION: <br /><textarea name="affirmations" type="textarea" defaultValue={journal.affirmations} /><br />
+                        TODAY: <br /><textarea name="today" type="textarea" defaultValue={journal.today} /><br />
+                        TOMORROW: <br /><textarea name="tomorrow" type="textarea"  defaultValue={journal.tomorrow} /><br />
 
-
-                        BRUSH TEETH:<input name="brushTeeth" type="checkbox" /><br />
-                        WASH FACE:<input name="washFace" type="checkbox" /><br />
-                        EAT MEAL:<input name="eatMeal" type="checkbox" /><br />
-                        GET FRESH AIR:<input name="getFreshAir" type="checkbox" /><br />
-                        DRINK WATER:<input name="drinkWater" type="checkbox" /><br />
+                        BRUSH TEETH: {this.props.journal.brushTeeth? <input name="brushTeeth" type="checkbox" defaultChecked /> : <input type="checkbox" name= "brushTeeth" />}
+                        WASH FACE: {this.props.journal.washFace? <input name="washFace" type="checkbox" defaultChecked /> : <input type="checkbox" name= "washFace" />}
+                        EAT MEAL: {this.props.journal.eatMeal? <input name="eatMeal" type="checkbox" defaultChecked /> : <input type="checkbox" name= "eatMeal" />}
+                        GET FRESH AIR: {this.props.journal.getFreshAir? <input name="getFreshAir" type="checkbox" defaultChecked /> : <input type="checkbox" name= "getFreshAir" />}
+                        DRINK WATER: {this.props.journal.drinkWater? <input name="drinkWater" type="checkbox" defaultChecked /> : <input type="checkbox" name= "eatMeal" />}
                     
                       
 
